b09fdde5902aa67ee65cae440914728da243ab62
davdev14 <davdev1400@gmail.com>
2023-06-21 17:04:39 +0100

    inheritance

--- inheritance/inheritance.js
+++ inheritance/inheritance.js
@@ -33,4 +33,34 @@ class Employee extends Person {
 }
 let employee = new Employee('dave', 'ouatedem', 'front-end developer');
 console.log(employee.describe());
+// abstract classes
+class EmployeeAbstractClass {
+    firstname;
+    lastname;
+    constructor(firstname, lastname) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+    }
+    getFullName() {
+        return `${this.firstname} ${this.lastname}`;
+    }
+    statement(greeting) {
+        return `${greeting} ${this.getFullName()} your salary is ${this.getSalary()}`;
+    }
+}
+class fullTimeEmployee extends EmployeeAbstractClass {
+    salary;
+    constructor(firstname, lastname, salary) {
+        super(firstname, lastname);
+        this.salary = salary;
+    }
+    getSalary() {
+        return this.salary;
+    }
+    statement() {
+        return `${this.getFullName()} ${super.statement('hello')}`;
+    }
+}
+let john = new fullTimeEmployee('john', 'doe', 500000);
+console.log(john.statement());
 //# sourceMappingURL=inheritance.js.map
--- inheritance/inheritance.ts
+++ inheritance/inheritance.ts
@@ -40,3 +40,32 @@ let employee = new Employee('dave', 'ouatedem', 'front-end developer')
 console.log(employee.describe());
 
 // abstract classes
+
+abstract class EmployeeAbstractClass {
+  constructor(private firstname: string, private lastname: string){
+    
+  }
+  abstract getSalary(): number
+  getFullName(): string{
+    return `${this.firstname} ${this.lastname}`
+  }
+  statement(greeting: string): string{
+    return `${greeting} ${this.getFullName()} your salary is ${this.getSalary()}`
+  }
+}
+
+class fullTimeEmployee extends EmployeeAbstractClass{
+  constructor(firstname:string,lastname:string, private salary: number){
+    super(firstname, lastname)
+  }
+  getSalary(): number {
+      return this.salary
+  }
+  statement(): string {
+    return `${super.statement('hello')}`
+      
+  }
+}
+let john = new fullTimeEmployee('john', 'doe', 500000)
+console.log(john.statement());
+
